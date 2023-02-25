3ebdb9852cc8292b26d91134664fee58c4459d6e
Aangfly <63817342+Aangfly@users.noreply.github.com>
2023-02-24 23:25:25 -0600

    Add footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import "./Footer.css"
+
+export const Footer = () => {
+  return (
+        <footer className="footer">
+
+            <div  className="footerCopy">
+                <p>Todos los derechos reservados a Hackathon</p>
+            </div>
+
+        </footer>
+  );
+}
+
+
