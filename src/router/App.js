c8f52fd81e89611e92da7bae635ae70c78638e2e
Javier Eduardo Vargas Medina <skill339@gmail.com>
2023-02-25 00:43:45 -0600

    changes

--- src/router/App.js
+++ src/router/App.js
@@ -17,6 +17,7 @@ function App() {
       }>
         <div className="App">
           <NavBar />
+          <Aside/>
           <Routes>
             {/* <Route path="/" element={<HomeView />} /> */}
           </Routes>
