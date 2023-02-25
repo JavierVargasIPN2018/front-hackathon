265bc2956024a98a471a5bd889b7bfabdde0ee4b
Javier Eduardo Vargas Medina <skill339@gmail.com>
2023-02-24 22:40:48 -0600

    add firts css

---
+++ src/assets/index.js
@@ -0,0 +1,1 @@
+export const logoNavbar=require('./logoNavbar.png')
--- src/components/navBar/navbar.jsx
+++ src/components/navBar/navbar.jsx
@@ -1,5 +1,16 @@
+import './navbar.css'
+import { logoNavbar } from '../../assets/'
 export const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className="navBar">
+      <div className='one'>
+      <img className='imgNavbar' src={logoNavbar} />
+      </div>
+      <div className='second'>
+        <h2>Registro</h2>
+        <h2>Información de la empresa</h2>
+        <h2>Contactos</h2>
+      </div>
+    </div>
   )
 }
