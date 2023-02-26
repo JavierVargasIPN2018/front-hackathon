1cf635f1564f4621679b3e652439ea8935f13bba
Aangfly <63817342+Aangfly@users.noreply.github.com>
2023-02-26 11:48:43 -0600

    Add description about hospital

--- src/components/Doctors/Doctor.jsx
+++ src/components/Doctors/Doctor.jsx
@@ -11,36 +11,28 @@ export const Doctor = () => {
             <li className="Doc"><img src={Doc4} alt="Doctor" /></li>
         </ul>
         <main className="hospitales contenedor">
-            <div className="nHos">
-                <h3>Hospital 1</h3>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                    Reprehenderit facere ipsa debitis modi, quam incidunt temporibus 
-                    architecto accusantium eligendi doloremque omnis voluptas perferendis 
-                    aliquid magnam perspiciatis, suscipit officiis sunt laborum?</p>
+            <div className="nHos contenedor">
+                <h3>Clínica Mayo</h3>
+                <p>Ciudad: Rochester (Minnesota, Estados Unidos)</p>
+                <p>Es el centro médico más referenciado por la comunidad médica mundial. Cubre todas las especialidades, incluidas las enfermedades más inusuales o raras y está a la cabeza de la innovación tecnológica y científica. Sus equipos de especialistas trabajan interconectados, lo que acelera la definición de diagnósticos.</p>
                     <img className="imgH" src={lHospital} alt="Logo Hospital" />
             </div>
-            <div className="nHos">
-                <h3>Hospital 2</h3>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                    Reprehenderit facere ipsa debitis modi, quam incidunt temporibus 
-                    architecto accusantium eligendi doloremque omnis voluptas perferendis 
-                    aliquid magnam perspiciatis, suscipit officiis sunt laborum?</p>
+            <div className="nHos contenedor">
+                <h3>Singapore General Hospital</h3>
+                <p>Ciudad: Singapur</p>
+                <p>Referencia para todo el sureste asiático. Está a la cabeza del empleo de las más avanzadas tecnologías en la asistencia médica y quirúrgica; tiene el mayor número de intervenciones quirúrgicas realizadas mediante robots.</p>
                 <img className="imgH" src={lHospital} alt="Logo Hospital" />
             </div>
-            <div className="nHos" >
-                <h3>Hospital 3</h3>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                    Reprehenderit facere ipsa debitis modi, quam incidunt temporibus 
-                    architecto accusantium eligendi doloremque omnis voluptas perferendis 
-                    aliquid magnam perspiciatis, suscipit officiis sunt laborum?</p>
+            <div className="nHos contenedor" >
+                <h3>Karolinska University Hospital</h3>
+                <p>Ciudad: Estocolmo (Suecia)</p>
+                <p>Centrado en la investigación biomédica y estrechamente relacionado con el Instituto Karolinska, cuyo comité científico designa cada año al Premio Nobel de Medicina.</p>
                 <img className="imgH" src={lHospital} alt="Logo Hospital" />
             </div>
-            <div className="nHos" >
-                <h3>Hospital 4</h3>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                    Reprehenderit facere ipsa debitis modi, quam incidunt temporibus 
-                    architecto accusantium eligendi doloremque omnis voluptas perferendis 
-                    aliquid magnam perspiciatis, suscipit officiis sunt laborum?</p>
+            <div className="nHos contenedor" >
+                <h3>Toronto General Hospital</h3>
+                <p>Ciudad: Toronto (Canadá)</p>
+                <p>Es el centro hospitalario de referencia mundial para los trasplantes de órganos: corazón riñón, pulmón, páncreas, hígado…, y sus procedimientos técnicos son seguidos por toda la comunidad quirúrgica internacional</p>
                 <img className="imgH" src={lHospital} alt="Logo Hospital" />
             </div>
         </main>
